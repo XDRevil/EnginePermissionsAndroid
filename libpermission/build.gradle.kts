0fa6d1ad228340d4001e27cf2413b98d99183340
XDRevil <xdrevil464@gmail.com>
2025-11-16 09:04:03 +0200

    0.2

--- libpermission/build.gradle.kts
+++ libpermission/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
-    id("maven-publish")  
+    id("maven-publish")
 }
 
 android {
@@ -13,31 +13,45 @@ android {
     }
 
     buildTypes {
-        release {
+        debug {
             isMinifyEnabled = false
+            applicationIdSuffix = ".debug"
+        }
+
+        release {
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            buildConfigField("String", "BUILD_TYPE", "\"release\"")
         }
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
 }
 
 afterEvaluate {
     publishing {
         publications {
             create<MavenPublication>("release") {
-                from(components["release"])
-
+                from(components.findByName("release") ?: throw GradleException("Component 'release' not found"))
                 groupId = "com.github.XDRevil"
                 artifactId = "EnginePermissionsAndroid"
-                version = "0.2"  
+                version = "0.2"
+            }
+        }
+
+        repositories {
+            maven {
+                name = "local"
+                url = uri("$buildDir/repos")
             }
         }
     }
 }
+
